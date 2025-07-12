99b3ff69f57f9ceadf79a76857a1f88a695bb0af
Ed Abbondanzio <me@eddieabbondanz.io>
2025-07-11 22:42:53 -0400

    Auto set up rollup for els

--- assets/widgets/vite.config.ts
+++ assets/widgets/vite.config.ts
@@ -5,6 +5,14 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import { resolve } from 'node:path'
+import { readdirSync } from 'node:fs'
+
+const elements = readdirSync(resolve(__dirname, "src/elements")).map(fileName => ({
+  fileName,
+  compiledFileName: fileName.split(".")[0]
+}))
+const rollupInputs: Record<string, string> = {}
+elements.forEach(el => rollupInputs[el.compiledFileName] = el.fileName)
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -21,10 +29,7 @@ export default defineConfig({
   build: {
     outDir: "dist/elements",
     rollupOptions: {
-      input: {
-        // TODO: Make this automatic
-        'b-series-gear-calculator': resolve(__dirname, "src/elements/b-series-gear-calculator.ts")
-      },
+      input: rollupInputs,
       output: {
         entryFileNames(chunkInfo) {
           return `${chunkInfo.name}.js`
