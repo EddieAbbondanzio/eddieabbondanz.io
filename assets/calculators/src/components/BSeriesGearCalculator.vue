45831bc394f91a992f882bf9e414714d598827dc
Ed Abbondanzio <me@eddieabbondanz.io>
2025-07-11 22:06:53 -0400

    Clean up vue code

--- assets/calculators/src/components/BSeriesGearCalculator.vue
+++ assets/calculators/src/components/BSeriesGearCalculator.vue
@@ -1,13 +1,11 @@
+
 <script setup lang="ts">
-console.log("FOO!");
+console.log("GEARESSSS!");
 </script>
 
 <template>
-<h1>WE DID IT!</h1>
+<h1>B-Series Gear Calculator Thingy</h1>
 </template>
 
 <style scoped>
-h1 {
-    background-color: red;
-}
 </style>
--- assets/calculators/src/elements/foo.ts
+++
@@ -1,6 +0,0 @@
-import { defineCustomElement } from 'vue'
-
-import Foo from '../components/Foo.vue'
-
-console.log("Register foo")
-customElements.define('foo-bar', defineCustomElement(Foo))
