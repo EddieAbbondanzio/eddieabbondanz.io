0bced9deb36e9a462ca0319b7a305a84a2c23491
Eddie Abbondanzio <me@eddieabbondanz.io>
2025-07-16 22:47:59 -0400

    Get vue SFC styles working (#5)

--- widgets/vite.config.ts
+++ widgets/vite.config.ts
@@ -6,81 +6,96 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import path from 'node:path'
 import { readdirSync } from 'node:fs'
-import { viteStaticCopy } from 'vite-plugin-static-copy';
-
-// Webcomponents
-const elementDirPath = path.resolve(__dirname, "src/elements")
-const elements = readdirSync(elementDirPath).map(fileName => ({
-  fileName,
-  compiledFileName: fileName.split(".")[0]
-}))
-const rollupInputs: Record<string, string> = {}
-elements.forEach(el => rollupInputs[el.compiledFileName] = path.join(elementDirPath, el.fileName))
-
-// Shoelace - Ref: https://github.com/shoelace-style/shoelace/discussions/1240
-const iconsPath = '../node_modules/@shoelace-style/shoelace/dist/assets/icons';
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    // Enable export SFCs as custom elements
-    vue({
-      features: { customElement: true }, template: {
-        compilerOptions: {
-          // Shoelace components
-          isCustomElement: tag => tag.startsWith("sl-")
-        }
-      }
-    }),
-    vueJsx(),
-    vueDevTools(),
-    // Copy assets to Vue build dir for demo use (has to be under root dir of Vue)
-    viteStaticCopy({
-      targets: [
-        {
-          src: iconsPath,
-          dest: 'assets',
+export default defineConfig(({ command }) => {
+  const isDevelopment = command === 'serve'
+
+  // Shoelace - Ref: https://github.com/shoelace-style/shoelace/discussions/1240
+  const iconsPath = '../node_modules/@shoelace-style/shoelace/dist/assets/icons'
+
+  // Webcomponents
+  const elementDirPath = path.resolve(__dirname, 'src/elements')
+  const elements = readdirSync(elementDirPath).map((fileName) => ({
+    fileName,
+    compiledFileName: fileName.split('.')[0],
+  }))
+  const rollupInputs: Record<string, string> = {}
+  elements.forEach(
+    (el) => (rollupInputs[el.compiledFileName] = path.join(elementDirPath, el.fileName)),
+  )
+
+  /*
+   * When `customElement: true` is set Vite will compile CSS directly into the
+   * JS so we don't have to worry about importing CSS files when using the built
+   * web components. However, this breaks CSS in dev so we only enable it when
+   * building.
+   */
+  const customElement = !isDevelopment
+
+  return {
+    plugins: [
+      // Enable export SFCs as custom elements
+      vue({
+        features: { customElement },
+        template: {
+          compilerOptions: {
+            delimiters: ['[[', ']]'],
+            // Shoelace components
+            isCustomElement: (tag) => tag.startsWith('sl-'),
+          },
         },
-      ],
-    }),
-    // Copy assets to Hugo static directory for blog use
-    viteStaticCopy({
-      targets: [
-        {
-          src: iconsPath,
-          dest: '../../static/shoelace/assets',
-        },
-      ],
-    }),
-  ],
-  resolve: {
-    alias: [
-      {
-        find: '@',
-        replacement: fileURLToPath(new URL('./src', import.meta.url))
-      },
-      // Shoelace icons
-      {
-        find: /\/assets\/icons\/(.+)/,
-        replacement: `${iconsPath}/$1`,
-      }
+      }),
+      vueJsx(),
+      vueDevTools(),
+      // Copy assets to Vue build dir for demo use (has to be under root dir of Vue)
+      viteStaticCopy({
+        targets: [
+          {
+            src: iconsPath,
+            dest: 'assets',
+          },
+        ],
+      }),
+      // Copy assets to Hugo static directory for blog use
+      viteStaticCopy({
+        targets: [
+          {
+            src: iconsPath,
+            dest: '../../static/shoelace/assets',
+          },
+        ],
+      }),
     ],
-  },
-  build: {
-    // Required since output dir is outside of vue project.
-    emptyOutDir: true,
-    rollupOptions: {
-      input: rollupInputs,
-      output: {
-        assetFileNames: (asset) => {
-          return '[name][extname]'
+    resolve: {
+      alias: [
+        {
+          find: '@',
+          replacement: fileURLToPath(new URL('./src', import.meta.url)),
         },
-        dir: "../static/widgets",
-        entryFileNames(chunkInfo) {
-          return `${chunkInfo.name}.js`
-        }
+        // Shoelace icons
+        {
+          find: /\/assets\/icons\/(.+)/,
+          replacement: `${iconsPath}/$1`,
+        },
+      ],
+    },
+    build: {
+      // Required since output dir is outside of vue project.
+      emptyOutDir: true,
+      rollupOptions: {
+        input: rollupInputs,
+        output: {
+          assetFileNames: (asset) => {
+            return '[name][extname]'
+          },
+          dir: '../static/widgets',
+          entryFileNames(chunkInfo) {
+            return `${chunkInfo.name}.js`
+          },
+        },
       },
     },
-  },
-
+  }
 })
