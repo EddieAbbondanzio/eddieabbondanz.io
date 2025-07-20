9c6689c12dcc71135e0a9815be61e1439742d5c5
Ed Abbondanzio <me@eddieabbondanz.io>
2025-07-20 18:28:14 -0400

    Disable graph redraw animation

--- widgets/src/components/b-series-gear-calculator/GearChart.vue
+++ widgets/src/components/b-series-gear-calculator/GearChart.vue
@@ -53,6 +53,9 @@
   });
 
   const options = computed(() => ({
+    animation: {
+      duration: 0,
+    },
     plugins: {
       legend: {
         display: false
@@ -89,30 +92,3 @@
 <template>
   <Scatter :data="data" :options="options" />
 </template>
-
-<!-- Make one 'dataset' per gear (5 total) -->
-<!-- X axis: mph -->
-<!-- Y axis: RPMs -->
-<!-- 
-const labels = Utils.months({count: 7});
-const data = {
-  labels: labels,
-  datasets: [
-  {
-    label: 'My First Dataset',
-    data: 
-      [65, 59, 80, 81, 56, 55, 40],
-    fill: false,
-    borderColor: 'rgb(75, 192, 192)',
-    tension: 0.1
-  },
-    {
-    label: 'My First Dataset',
-    data: 
-      [20, 40],
-    fill: false,
-    borderColor: 'rgb(75, 192, 192)',
-    tension: 0.1
-  },
-  ]
-}; -->
